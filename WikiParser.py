c11ed4348506e0c73fc29b4b3465cdf49832d0d3
AsteXOriginal <117456510+AsteXOriginal@users.noreply.github.com>
2023-05-23 18:03:24 +0300

    Update WikiParser.py

--- WikiParser.py
+++ WikiParser.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 
-url = 'https://uk.wikipedia.org/wiki/%D0%94%D0%B8%D0%BD%D0%BE%D0%B7%D0%B0%D0%B2%D1%80%D0%B8' # потрібно вставити посилання сторінки
+url = '' <-- #Paste the page link
 
 response = requests.get(url)
 
