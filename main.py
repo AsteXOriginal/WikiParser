f8a7474335359af3a9d14eca678a3e91862c0c17
AsteXOriginal <117456510+AsteXOriginal@users.noreply.github.com>
2023-05-23 18:31:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 
+try:
 
-url = '' <-- # Paste the page link to see the title
+    url = str(input('Paste link --> '))
 
 
-response = requests.get(url)
 
-if response.status_code == 200:
-    result = BeautifulSoup(response.text, 'html.parser')
-    title = result.find('h1').text
+    response = requests.get(url)
 
-    print(f"Заголовок сторінки: {title}")
-else:
-    print("error connect")
+    if response.status_code == 200:
+        result = BeautifulSoup(response.text, 'html.parser')
+        title = result.select('p')
+        for items in title:
+            print(items.text)
+    
+    else:
+        print("error connect")
+except ValueError:
+    print("Please, paste link")
