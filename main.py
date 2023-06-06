3a679fa4977455bb9f7774489b986bceb0391ed2
AsteXOriginal <117456510+AsteXOriginal@users.noreply.github.com>
2023-06-06 21:29:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
-
 try:
-
     url = str(input('Paste link --> '))
-
-
-
     response = requests.get(url)
-
     if response.status_code == 200:
         result = BeautifulSoup(response.text, 'html.parser')
         title = result.select('p')
         for items in title:
             print(items.text)
-    
     else:
-        print("error connect")
+        print("error connect")        
 except ValueError:
     print("Please, paste link")
